876cc139f6bedcab4006cb3e6aa55291e04a61da
Rafael Alcântara <r4f4elr@gmail.com>
2022-10-11 22:19:02 -0300

    Testes

--- src/main/java/dio/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/dio/digitalinnovation/parking/service/ParkingService.java
@@ -1,5 +1,6 @@
 package dio.digitalinnovation.parking.service;
 
+import dio.digitalinnovation.parking.exception.ParkingNotFoundException;
 import dio.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,7 @@ import java.util.stream.Collectors;
 @Service
 public class ParkingService {
     private static Map<String, Parking> parkingMap = new HashMap();
-    static {
-        var id  = getUUID();
-        var id1  = getUUID();
-        Parking parking = new Parking(id, "DMS-1111","SC", "CELTA", "PRETO");
-        Parking parking1 = new Parking(id1, "WAS-1234","SP", "VW GOL", "VERMELHO");
-        parkingMap.put(id, parking);
-        parkingMap.put(id1, parking1);
-    }
+
     public List<Parking> findAll(){
         return parkingMap.values().stream().collect(Collectors.toList());
     }
@@ -27,7 +21,11 @@ public class ParkingService {
     }
 
     public Parking findById(String id) {
-        return parkingMap.get(id);
+        Parking parking = parkingMap.get(id);
+        if (parking == null){
+            throw  new ParkingNotFoundException(id);
+        }
+        return parking;
     }
 
     public Parking create(Parking parkingCreate) {
@@ -36,5 +34,25 @@ public class ParkingService {
         parkingCreate.setEntryDate(LocalDateTime.now());
         parkingMap.put(uuid, parkingCreate);
         return parkingCreate;
+    }
+
+    public void delete(String id) {
+        Parking parking = findById(id);
+        parkingMap.remove(id);
+    }
+
+    public Parking update(String id, Parking parkingCreate) {
+        Parking parking = findById(id);
+        parking.setColor(parkingCreate.getColor());
+        parkingMap.replace(id, parking);
+        return  parking;
+    }
+
+    public Parking exit(String id) {
+        //reccuperar o estacionado
+        //atualizar data de saida
+        //calcular o valor
+
+        return null;
     }
 }
